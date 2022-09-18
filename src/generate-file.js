2e3a34b0de18087525258e7d9248dc048caaf03e
Egg <adrianjimenez1950@gmail.com>
2022-09-18 08:51:10 -0400

    Created template html and css, and added them to template.js

---
+++ src/generate-file.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/index.html', fileContent, err => {
+        if(err) {
+            reject(err)
+            return;
+        } 
+
+        resolve({
+            ok: true,
+            message: `
+            ===================
+            index.html created!
+            ===================
+            `
+        })
+    })
+}) 
+};
+
+module.exports = { writeFile };
